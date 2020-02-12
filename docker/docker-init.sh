a9e762ea6860b1990368737fd736fc6eb10e8e87
Rajat Gupta <gupta_rajat@optum.com>
2020-02-12 15:42:42 +0530

    change default password

--- docker/docker-init.sh
+++ docker/docker-init.sh
@@ -40,7 +40,7 @@ superset fab create-admin \
               --firstname Superset \
               --lastname Admin \
               --email admin@superset.com \
-              --password admin
+              --password PassWord
 echo_step "1" "Complete" "Setting up admin user"
 
 # Initialize the database
